dc88de07837518639817af9b91d7e7e196ea5a66
JorgeJDev <jjuansanda@gmail.com>
2024-02-23 16:14:50 +0100

    refactor update manage.sh

--- manage.sh
+++ manage.sh
@@ -2,18 +2,21 @@
 # manage.sh
 
 # Espera a que la base de datos esté lista
-echo "Esperando a que la base de datos esté lista..."
+echo "Waiting for db to be ready..."
+while ! nc -z db 5432; do
+  sleep 0.1
+done
 
-echo "La base de datos está lista."
+echo "DB ready."
 
 # Configuración del entorno
 export FLASK_ENV=development
 export DATABASE_URI=postgresql://postgres:admin@db:5432/proyecto_final_DO
 
 # Inicializa la base de datos
-echo "Inicializando la base de datos..."
+echo "Creating tables in DB..."
 python manage.py
 
 # Inicia la aplicación Flask
-echo "Iniciando la aplicación Flask..."
+echo "Initiating APP..."
 python run.py
